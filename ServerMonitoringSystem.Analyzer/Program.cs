21519299ba2ce24776483b677a89c9cd2fd05bf8
Izzat Alsharif <work@izzatalsharif.com>
2024-06-02 22:53:18 +0300

    Configure programs with graceful shutdown

--- ServerMonitoringSystem.Analyzer/Program.cs
+++ ServerMonitoringSystem.Analyzer/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using ServerMonitoringSystem.Analyzer.Configuration;
 using ServerMonitoringSystem.Analyzer.Interfaces;
+using ServerMonitoringSystem.Common.Interfaces;
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -11,7 +12,39 @@ var configuration = new ConfigurationBuilder()
 var services = new ServiceCollection();
 services.InjectConfigurationServices(configuration);
 services.InjectSignalRAlerterServices();
+services.InjectRabbitMqConsumerServices();
 var provider = services.BuildServiceProvider();
 
 var alerter = provider.GetRequiredService<IAlertSender>();
 await alerter.SendAlertAsync("Example alert");
+
+var consumer = provider.GetRequiredService<IMessageConsumer>();
+var cts = new CancellationTokenSource();
+
+AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+{
+    Console.WriteLine("Process exiting...");
+    cts.Cancel();
+};
+
+Console.CancelKeyPress += (_, e) =>
+{
+    Console.WriteLine("Canceling...");
+    cts.Cancel();
+    e.Cancel = true;
+};
+
+consumer.StartConsuming(async message =>
+{
+    Console.WriteLine(message);
+    await Task.CompletedTask;
+}, cts.Token);
+
+Console.WriteLine("Connection successful");
+Console.WriteLine("Press [Enter] to exit.");
+await Task.Delay(-1, cts.Token);
+
+cts.Token.WaitHandle.WaitOne();
+Console.WriteLine("Consumer stopped.");
+
+provider.Dispose();
--- ServerMonitoringSystem.Collector/Program.cs
+++ ServerMonitoringSystem.Collector/Program.cs
@@ -19,6 +19,20 @@ var provider = services.BuildServiceProvider();
 var generator = provider.GetRequiredService<IServerStatisticsGenerator>();
 var clock = provider.GetRequiredService<IClockService>();
 var producer = provider.GetRequiredService<IMessageProducer>();
+var cts = new CancellationTokenSource();
+
+AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+{
+    Console.WriteLine("Process exiting...");
+    cts.Cancel();
+};
+
+Console.CancelKeyPress += (_, e) =>
+{
+    Console.WriteLine("Canceling...");
+    cts.Cancel();
+    e.Cancel = true;
+};
 
 await clock.StartAsync(
     async () =>
@@ -27,4 +41,11 @@ await clock.StartAsync(
         Console.WriteLine(message);
         producer.SendMessage(message);
     },
-    CancellationToken.None);
+    cts.Token);
+
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
+
+cts.Cancel();
+provider.Dispose();
+Console.WriteLine("Application stopped.");
